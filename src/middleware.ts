f4b62a0d508b8e64dbacaa5f365023a5aa5baf79
bilalcodes-webdev <bilal.webdev.official@gmail.com>
2025-08-17 16:05:32 +0500

    middleware configure

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,7 +45,7 @@ import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
 export const config = {
   // matcher tells Next.js which routes to run the middleware on.
   // This runs the middleware on all routes except for static assets.
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth|api/webhooks/stripe).*)"],
 };
 
 // Pass any existing middleware with the optional existingMiddleware prop
